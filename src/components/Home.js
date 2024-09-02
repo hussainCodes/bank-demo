65265a3bd00ec72bd2172485776f5147e14f18e3
khaled07-ag <khaled-kk01@hotmail.com>
2024-09-02 20:30:30 +0300

    components

---
+++ src/components/Home.js
@@ -0,0 +1,18 @@
+
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div>
+        Balance 
+    </div>
+    <div>
+        make a transaction
+    </div>
+    </>
+  )
+}
+
+export default Home
+
