af2b3a9a91b2d7f6cb2c0ad058982b0b18bbc9ac
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-09-03 19:51:46 +0300

    finish user context

--- src/App.js
+++ src/App.js
@@ -1,14 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
-import Profile from './components/Profile';
-import Navbar from './components/Navbar';
-import { Outlet } from 'react-router-dom';
+import logo from "./logo.svg";
+import "./App.css";
+import Profile from "./components/Profile";
+import Navbar from "./components/Navbar";
+import { Outlet } from "react-router-dom";
+import { useState } from "react";
+import UserContext from "./Context/UserContext";
+
 function App() {
+  const [user, setUser] = useState(false);
   return (
-    <div >
-      <Navbar/>
-     <Outlet/>
-    </div>
+    <UserContext.Provider value={[user, setUser]}>
+      <div>
+        <Navbar />
+        <Outlet />
+      </div>
+    </UserContext.Provider>
   );
 }
 
