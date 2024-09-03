106e7ae35797d56628e93ebac5cc9552f47d90e3
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-09-03 08:22:23 +0300

    add filter fieldset css

--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -4,26 +4,33 @@ const Transactions = () => {
   return (
     <>
       <div>
-        <fieldset>
-          <legend>Filter:</legend>
-          <div>
-            <input type="radio" id="all" name="filter" value="all" checked />
-            <label for="all">All</label>
-          </div>
-          <div>
-            <input type="radio" id="deposits" name="filter" value="deposits" />
-            <label for="deposits">Deposits</label>
-          </div>
-          <div>
-            <input
-              type="radio"
-              id="withdraws"
-              name="filter"
-              value="withdraws"
-            />
-            <label for="withdraws">Withdraws</label>
-          </div>
-        </fieldset>
+        <div className="filter-fieldset-container">
+          <fieldset>
+            <legend>Filter:</legend>
+            <div>
+              <input type="radio" id="all" name="filter" value="all" checked />
+              <label for="all">All</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="deposits"
+                name="filter"
+                value="deposits"
+              />
+              <label for="deposits">Deposits</label>
+            </div>
+            <div>
+              <input
+                type="radio"
+                id="withdraws"
+                name="filter"
+                value="withdraws"
+              />
+              <label for="withdraws">Withdraws</label>
+            </div>
+          </fieldset>
+        </div>
       </div>
     </>
   );
