5e916af7cd01311e6b2d8f89c10564312dd4731e
khaled07-ag <khaled-kk01@hotmail.com>
2024-09-03 17:59:24 +0300

    router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Profile from './components/Profile';
+import Navbar from './components/Navbar';
+import { Outlet } from 'react-router-dom';
 function App() {
   return (
-    <div className="App">
-      
-     
+    <div >
+      <Navbar/>
+     <Outlet/>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,11 +3,56 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-
+import {createBrowserRouter, RouterProvider} from "react-router-dom"
+import Home from './components/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Transactions from './components/Transactions';
+import Users from './components/Users';
+import Profile from './components/Profile';
+import Account from './components/Account';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+const router = createBrowserRouter([
+  {
+  path:"/",
+  element:<App/>,
+  children:[
+    {
+      path:"/",
+      element:<Home/>
+    },
+    {
+      path:"/login",
+      element:<Login/>
+    },
+    {
+      path:"/register",
+      element:<Register/>
+    },
+    {
+      path:"/transactions",
+      element:<Transactions/>
+    },
+    {
+      path:"/users",
+      element:<Users/>
+    },
+    {
+      path:"/profile/:id",
+      element:<Profile/>
+    },
+    {
+      path:"/Account",
+      element:<Account/>
+    }
+
+
+  ]
+}
+])
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
 
