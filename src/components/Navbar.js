7f61211f4a1688aa6801ee8c0a1e95403c5633b6
khaled07-ag <khaled-kk01@hotmail.com>
2024-09-03 18:56:42 +0300

    router

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='bg-gray-500 '>
+        <div className='bg-green-500 '>
+        <h1 className='font-bold text-green-600'>Bank</h1>
+        </div>
+    </div>
   )
 }
 
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,8 +1,13 @@
 import React from 'react'
 
 const Register = () => {
+
   return (
-    <div>Register</div>
+    <div className='bg-blue-200 justify-center flex'>
+        
+       <h1 ></h1> 
+
+    </div>
   )
 }
 
