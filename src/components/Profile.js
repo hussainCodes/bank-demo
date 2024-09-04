546e830d633565e625a8c530b0ac0f4f92167742
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-09-04 17:45:08 +0300

    flex profile card

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,16 +3,20 @@ import React from "react";
 const Profile = () => {
   return (
     <>
-      <div className="profile-container">
-        <div className="bg-black">
-          <img
-            src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
-            alt="profile picture"
-          />
-          <h1>Name</h1>
-          <h3>balance</h3>
-          <input type="file" accept="image/*" />
-          <button>Save</button>
+      <div className="flex justify-center mt-20 h-[100vh]">
+        <div className="w-[75] border border-dashed h-80">
+          <div>
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/7/7c/Profile_avatar_placeholder_large.png?20150327203541"
+              alt="profile picture"
+            />
+          </div>
+          <div className="flex flex-col items-center gap-1">
+            <h1>Name</h1>
+            <h3>balance</h3>
+            <input type="file" accept="image/*" />
+            <button>Save</button>
+          </div>
         </div>
       </div>
     </>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,65 +1,54 @@
-import React,{ useState } from 'react'
-import {useMutation} from "@tanstack/react-query"
-import { registerUser } from '../API/auth'
+import React, { useState } from "react";
+import { useMutation } from "@tanstack/react-query";
+import { registerUser } from "../API/auth";
 const Register = () => {
-const [user, setUser] = useState({});
-const handleChange = (e)=>{
-    if(e.target.name ==="image"){
-        setUser({...user , [e.target.name]:e.target.files[0]})
-    }else{
-        setUser({...user, [e.target.name]:e.target.value})
+  const [user, setUser] = useState({});
+  const handleChange = (e) => {
+    if (e.target.name === "image") {
+      setUser({ ...user, [e.target.name]: e.target.files[0] });
+    } else {
+      setUser({ ...user, [e.target.name]: e.target.value });
     }
-}
-const {mutate} = useMutation({
-    mutationKey:["register"],
-    mutationFn:()=>registerUser(user)
-
-    
-}) 
-const handleSubmit = (e)=>{
-    console.log(e)
-    e.preventDefault()
+  };
+  const { mutate } = useMutation({
+    mutationKey: ["register"],
+    mutationFn: () => registerUser(user),
+  });
+  const handleSubmit = (e) => {
+    console.log(e);
+    e.preventDefault();
     console.log(user);
     mutate();
-    
-}
+  };
 
   return (
-    <div className='bg-blue-300 justify-center flex items-center h-[100vh]'>
-        <div className='border p-5 rounded-xl bg-white shadow-2xl'>
+    <div className="bg-blue-300 justify-center flex items-center h-[100vh]">
+      <div className="border p-5 rounded-xl bg-white shadow-2xl">
         <form onSubmit={handleSubmit}>
-       <div className='flex flex-col gap-2 '>
-       <h1 >Register</h1> 
-       <div>
-        <label>Username: </label>
-       <input
-       id='username'
-       type='name'
-       onChange={handleChange} 
-       />
-       </div>
-       <div>
-        <label>Password: </label>
-       <input
-       id='password'
-       type="password"
-       onChange={handleChange}
-       className=''
-       />
-       </div>
-       <input
-       type="file" accept="image/*" />
-       <button
-       type='submit'
-       className=''>
-        Register
-       </button>
-       </div>
-       </form>
-       </div>
-       
+          <div className="flex flex-col gap-2 ">
+            <h1>Register</h1>
+            <div>
+              <label>Username: </label>
+              <input id="username" type="name" onChange={handleChange} />
+            </div>
+            <div>
+              <label>Password: </label>
+              <input
+                id="password"
+                type="password"
+                onChange={handleChange}
+                className=""
+              />
+            </div>
+            <input type="file" accept="image/*" />
+            <button type="submit" className="">
+              Register
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
