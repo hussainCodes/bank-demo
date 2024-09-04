696c79c55ee6590bbf489e357af34cbc6e7922fc
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-09-04 08:57:01 +0300

    finish account page styling

--- src/components/Account.js
+++ src/components/Account.js
@@ -3,38 +3,51 @@ import React from "react";
 const Account = () => {
   return (
     <>
-      <div>
-        <div>
-          <h1>Your balance:</h1>
-          <h3>30,000.00</h3>
+      <div className="flex flex-col items-center justify-center h-[100vh] gap-20 font-serif">
+        <div className="flex flex-col items-center border border-dashed border-gray-400 p-16 rounded-2xl">
+          <h1 className="text-lg">Your balance:</h1>
+          <h3 className="text-3xl">30,000.00</h3>
         </div>
         <div>
-          <fieldset>
+          <fieldset className="border border-dashed border-gray-400 p-10 rounded-2xl font-serif">
             <legend>Make a transaction:</legend>
-            <div>
-              <input
-                type="radio"
-                id="deposit"
-                name="transaction"
-                value="deposit"
-                checked
-              />
-              <label for="deposit">Deposit</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="withdraw"
-                name="transaction"
-                value="withdraw"
-              />
-              <label for="withdraw">Withdraw</label>
-            </div>
-            <div>
-              <input placeholder="Enter the amount ..." />
-            </div>
-            <div>
-              <button>Submit</button>
+            <div className="flex flex-col gap-5">
+              <div className="flex justify-center gap-5">
+                <div>
+                  <input
+                    className=""
+                    type="radio"
+                    id="deposit"
+                    name="transaction"
+                    value="deposit"
+                    checked
+                  />
+                  <label for="deposit">Deposit</label>
+                </div>
+                <div>
+                  <input
+                    type="radio"
+                    id="withdraw"
+                    name="transaction"
+                    value="withdraw"
+                  />
+                  <label for="withdraw">Withdraw</label>
+                </div>
+              </div>
+              <div>
+                <input
+                  className="border border-black-100  rounded-sm "
+                  placeholder="Enter the amount ..."
+                />
+              </div>
+              <div>
+                <button
+                  className="w-full rounded-md bg-slate-800 py-2 px-4 border border-transparent text-center text-sm text-white transition-all shadow-md hover:shadow-lg focus:bg-slate-700 focus:shadow-none active:bg-slate-700 hover:bg-slate-700 active:shadow-none disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none ml-2"
+                  type="button"
+                >
+                  Submit
+                </button>
+              </div>
             </div>
           </fieldset>
         </div>
