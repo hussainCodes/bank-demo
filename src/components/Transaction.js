622ecbee1d5a9d29e6f6536dbd832998c1bc4a97
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-09-05 20:46:12 +0300

    finish transactions ui

---
+++ src/components/Transaction.js
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+
+const Transaction = ({ transaction }) => {
+  const plusOrMinus = transaction.type == "withdraw" ? "-" : "+";
+
+  if (transaction.type == "withdraw") {
+    return (
+      <>
+        <div className="flex justify-between p-5 items-center border border-r-2 w-96 rounded-xl h-14 border-red-500">
+          <div className="flex">
+            <div>{plusOrMinus}</div>
+            <div>{transaction.amount}</div>
+          </div>
+          <div>{transaction.date}</div>
+          <div>{transaction.type}</div>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <>
+      <div className="flex justify-between p-5 items-center border w-96 rounded-xl h-14 border-green-500">
+        <div className="flex">
+          <div>{plusOrMinus}</div>
+          <div>{transaction.amount}</div>
+        </div>
+        <div>{transaction.date}</div>
+        <div>{transaction.type}</div>
+      </div>
+    </>
+  );
+};
+
+export default Transaction;
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,35 +1,61 @@
 import React from "react";
+import Transaction from "./Transaction";
+import { getMyTransactions } from "../API/auth";
 
 const Transactions = () => {
+  const transactions = [
+    {
+      amount: 200,
+      date: "10-7-1878",
+      type: "withdraw",
+    },
+    {
+      amount: 548,
+      date: "2-9-2055",
+      type: "deposit",
+    },
+    {
+      amount: 120,
+      date: "1-12-2030",
+      type: "withdraw",
+    },
+  ];
+
+  //   const {data: transactions} = useQuery({
+  //     queryKey: ["getMyTransactions"],
+  //     queryFn: getMyTransactions
+  // })
+
+  const transactionsList = transactions.map((transaction) => (
+    <Transaction transaction={transaction} />
+  ));
+
   return (
     <>
       <div>
-        <div className="filter-fieldset-container">
-          <fieldset>
-            <legend>Filter:</legend>
-            <div>
-              <input type="radio" id="all" name="filter" value="all" checked />
-              <label for="all">All</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="deposits"
-                name="filter"
-                value="deposits"
-              />
-              <label for="deposits">Deposits</label>
-            </div>
-            <div>
-              <input
-                type="radio"
-                id="withdraws"
-                name="filter"
-                value="withdraws"
-              />
-              <label for="withdraws">Withdraws</label>
-            </div>
-          </fieldset>
+        <fieldset className="flex">
+          <legend>Filter:</legend>
+          <div>
+            <input type="radio" id="all" name="filter" value="all" checked />
+            <label for="all">All</label>
+          </div>
+          <div>
+            <input type="radio" id="deposits" name="filter" value="deposits" />
+            <label for="deposits">Deposits</label>
+          </div>
+          <div>
+            <input
+              type="radio"
+              id="withdraws"
+              name="filter"
+              value="withdraws"
+            />
+            <label for="withdraws">Withdraws</label>
+          </div>
+        </fieldset>
+
+        <div className="flex flex-col items-center gap-10 mt-10">
+          {transactionsList}
         </div>
       </div>
     </>
