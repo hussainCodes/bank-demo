502a4f5e21fd573f8f5950299fa4159c5be2af0f
Hussain Alhaddad <hussain.alhaddad777@gmail.com>
2024-09-08 07:45:02 +0300

    make the links in home menu

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,12 +6,12 @@ const Home = () => {
     <>
       <div className="flex flex-col gap-5 mt-5 items-center">
         <div className="flex gap-5">
-          <HomeButton name={"Account"} />
-          <HomeButton name={"Transactions"} />
+          <HomeButton name={"Account"} direction={"Account"} />
+          <HomeButton name={"Transactions"} direction={"Transactions"} />
         </div>
         <div className="flex gap-5">
-          <HomeButton name={"Profile"} />
-          <HomeButton name={"Users"} />
+          <HomeButton name={"Profile"} direction={"Profile"} />
+          <HomeButton name={"Users"} direction={"Users"} />
         </div>
       </div>
     </>
